c5357c517dbb0cb4a2c2d33dbab9b10cdddc4f7b
Torino1564 <jtorinor6@gmail.com>
2024-11-28 19:03:21 -0300

    NO ANDA

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,90 @@
 #include <Arduino.h>
 #include <PID_v1.h>
 
-// pins
+// Pins
+static constexpr auto ADC_PIN = 13;  // Pin analógico de entrada
+static constexpr auto PWM_PIN = 32; 
+static constexpr auto IN1_PIN = 21;
+static constexpr auto IN2_PIN = 25;
+// Variables
+static double Vref = 1.5f;
+static double Vin = 0;          // Lectura de tensión
 
-static constexpr auto ADC_PIN = 13;
-static constexpr auto PWM_PIN = 32;
+static double Dref = 500;
+static double Din = 0;          // Lectura de tensión
+static double filteredDin = 0;  // Tensión suavizada
 
-// variables
+static double Vo = 0;
+static double filteredVin = 0;  // Tensión suavizada
+static double alpha = 0.3;      // Factor de suavizado (0 < alpha <= 1)
 
-static double maxSpeed = 0.8;
-static double dutyCycle = 0.5;
-static double sensorReading = 0;
-
-enum Direction{
+// Para la dirección del movimiento (si es necesario)
+enum Direction {
   FORWARD,
   BACKWARD
-  };
+};
 
 static Direction direction = Direction::FORWARD;
-static double Vin = 0;
-double controller(double);
+
+void controller();
+void planta();
+
+// Filtro de paso bajo (low-pass)
+double lowPassFilter(double input, double alpha, double* pVal) {
+  // Aplicar filtro de paso bajo exponencial
+  *pVal = (alpha * input) + ((1.0 - alpha) * *pVal);
+  return *pVal;
+}
+
+static double Kp = 1, Kd = 100, Ki = 200;
+PID controlador(&Din, &Vo, &Dref, Kp, Ki, Kd, DIRECT);
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(ADC_PIN, ANALOG);
-  pinMode(PWM_PIN, ANALOG);
-  analogReadResolution(10);
+  // Configuración inicial
+  Serial.begin(9600);   // Iniciar comunicación serial
+  pinMode(ADC_PIN, INPUT);  // Establecer pin de lectura como entrada
+  pinMode(PWM_PIN, OUTPUT); // Establecer pin de salida PWM (si lo usas)
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  controlador.SetOutputLimits(-100, 100);
+  controlador.SetMode(AUTOMATIC);
+  controlador.SetSampleTime(10);
+  analogReadResolution(10); // Resolución de 10 bits para el ADC (0-1023)
 }
 
 void loop() {
-  Vin = analogReadMilliVolts(ADC_PIN);
-  Serial.println("Tension ADC:");
-  Serial.print(Vin);
+  controller();
+  planta();
+}
+
+void controller()
+{
+  Din = analogRead(ADC_PIN);
+  filteredDin = lowPassFilter(Din, alpha, &filteredDin);
+  // if (filteredVin > 3 || filteredVin < 1)
+  //   filteredVin = 2.5;
+  Serial.print(" V\nFiltered Value: ");
+  Serial.print(filteredDin);
+  Serial.print(" V");
+  Serial.print(" \nVo: ");
+  Serial.print(Vo);
+  Serial.print("\n");
+
+  controlador.Compute();
+}
+
+void planta()
+{
+  if (Vo > 0)
+  {
+    digitalWrite(IN1_PIN, HIGH);
+    digitalWrite(IN2_PIN, LOW);
+  }
+  else 
+  {
+    digitalWrite(IN1_PIN, LOW);
+    digitalWrite(IN2_PIN, HIGH);
+  }
+  auto modVo = fabs(Vo);
+  analogWrite(PWM_PIN, modVo);
 }
